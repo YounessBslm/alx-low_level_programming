304377cbe26b764c6871302ff6dcac18f8dd18dc
youness <you.bassalem@gmail.com>
2023-07-27 17:06:25 -0700

    done

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,14 +1,13 @@
 #ifndef LISTS_H
 #define LISTS_H
+#include <stdio.h>
 
 /**
-* list_s - singly linked list 
-* @str: string - (malloc string)
+* struct list_s - singly linked list
+* @str: string - malloc string
 * @len: length of the string
 * @next: point to the next node
-*
 * Description: singly linked list node structure
-* for Holberton project
 */
 typedef struct list_s
 {
